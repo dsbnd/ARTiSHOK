768e76844d97d4829c89b3e6430441a13b290c98
dsbnd <dashabond07@mail.ru>
2025-11-19 14:09:04 +0300

    users

--- src/main/java/lab1/entities/User.java
+++ src/main/java/lab1/entities/User.java
@@ -1,10 +1,15 @@
 package lab1.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
-
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
